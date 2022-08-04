8e47ef9c011234626b15a5615ac0920d312cbd97
hithavedvvik <kavithabl88@gmail.com>
2022-08-04 12:11:23 +0530

    new test added

--- src/test/java/mavan_sde363/Gitcommab.java
+++ src/test/java/mavan_sde363/Gitcommab.java
@@ -1,10 +1,23 @@
 package mavan_sde363;
 
+import org.testng.annotations.Test;
+
 public class Gitcommab {
 	
-	public static void main(String[] args) {
-		System.out.println("hai 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");
-	}
+	@Test
+	public void practice1Test() {
+		String PASSWORD= System.getProperty("password");
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USERNAM = System.getProperty("username");
+		String PASSWORD1 = System.getProperty("password");
+		System.out.println("Test1");
+		System.out.println("Test1"+URL);
+		System.out.println("Test1"+BROWSER);
+		System.out.println("Test1"+USERNAM);
+		System.out.println("Test1"+PASSWORD);
+		System.out.println("Test1"+PASSWORD1);
 	
 
+}
 }
